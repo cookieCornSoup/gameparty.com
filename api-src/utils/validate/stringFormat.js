ec844ea0429b750de11864a3122e16eb5cc4920e
Zero Is Black <shlifedev@gmail.com>
2021-03-07 03:15:06 +0900

    Update stringFormat.js

--- api-src/utils/validate/stringFormat.js
+++ api-src/utils/validate/stringFormat.js
@@ -3,12 +3,16 @@
 // author email : shlifedev@gmail.com
 
 /* 올바른 이메일 양식인가? */
-function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+class StringFormatValidator{ 
+    validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    /* 숫자만 있는가? */
+    validateNumberOnlyString(str) {
+        const re = /^[0-9]*$/
+        return re.test(String(str));
+    }
 }
-/* 숫자만 있는가? */
-function validateNumberOnlyString(str) {
-    const re = /^[0-9]*$/
-    return re.test(String(str));
-}
+ 
+module.exports = new StringFormatValidator();
