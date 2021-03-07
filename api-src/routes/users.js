3bb8440b07d6f6373b031de2f1b26d9791de00da
Zero Is Black <shlifedev@gmail.com>
2021-03-07 16:54:30 +0900

    Create users.js

---
+++ api-src/routes/users.js
@@ -0,0 +1,22 @@
+// created by shlifedev at 20210307 03:10.
+// 
+// author email : shlifedev@gmail.com
+
+const express  = require('express');
+const router   = express.Router();
+const UserController = require('../controller/userController');
+// 라우터 URL 지정
+const URL = "/api";
+
+router.get('/users/:id', function(req,res){
+  let id = req.params.id;
+  res.send(id); 
+});
+
+router.post('/users/', UserController.signUp);
+
+
+module.exports ={
+  router,
+  url : URL
+} ;
