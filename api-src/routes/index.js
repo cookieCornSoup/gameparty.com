7d64fdac92334e3ad1b38c7968754daecb3500ae
Zero Is Black <shlifedev@gmail.com>
2021-03-07 16:54:33 +0900

    Update index.js

--- api-src/routes/index.js
+++ api-src/routes/index.js
@@ -1,24 +1,17 @@
+// created by shlifedev at 20210307 03:10.
+// 
+// author email : shlifedev@gmail.com
+
 /* 모든 라우터를 읽어서 배열형태로 exports 해주자. 그 외 라우터의 정보를 포함한다. */ 
 const fs = require('fs');
 const path = require('path'); 
 const routes = [];
+ 
 
-const apiRouter = path.join(__dirname, "api-router");
-const viewRouter = path.join(__dirname, "view-router");
-
-
-
-fs.readdirSync(apiRouter).filter(file =>{
+fs.readdirSync(__dirname).filter(file =>{
     return (file.indexOf('.') !== 0) && (file !== "index.js") && (file.slice(-3) === '.js');
 }).forEach(file =>{
-    routes.push(require(path.join(apiRouter, file))); 
-    console.log(file, ' 라우터 자동등록 ');
-});
-
-fs.readdirSync(viewRouter).filter(file =>{
-    return (file.indexOf('.') !== 0) && (file !== "index.js") && (file.slice(-3) === '.js');
-}).forEach(file =>{
-    routes.push(require(path.join(viewRouter, file))); 
+    routes.push(require(path.join(__dirname, file))); 
     console.log(file, ' 라우터 자동등록 ');
 });
  
