edd4eda5cb797d4591af9789f21467c2aa9f6805
Zero Is Black <shlifedev@gmail.com>
2021-03-07 16:54:53 +0900

    Update userController.js

--- api-src/controller/userController.js
+++ api-src/controller/userController.js
@@ -2,9 +2,37 @@
 // 
 // author email : shlifedev@gmail.com
 /* author : shlifedev */ 
-class UserController{
-    async function (req, res, next){
 
+const StringUtil = require('../utils/validate/stringFormat');
+const PasswordHelper = require('../utils/helper/passwordHelper'); 
+const UserService = require('../services/userService');
+
+
+class UserController{
+    async signIn (req, res, next){
+        let email = req.body.email;
+        let pw = req.body.password; 
+        if(StringUtil.validateEmail(email)){
+            UserService.create(email,pw);
+        }else{
+            res.statusCode = 400;
+        }
+       
+        
+    }
+    async signUp (req, res, next){ 
+        if(req.body.email && req.body.password){
+            let encryptResult = PasswordHelper.encrypt(req.body.password);
+            try{
+                await UserService.create(req.body.email, encryptResult);
+            }
+            catch(err){
+
+            } 
+        }
+  
+    }
+    async signOut (req, res, next){ 
     }
 }
 
