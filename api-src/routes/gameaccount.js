45289cc8639533058bdd34b2043faa66e27bb399
Zero Is Black <shlifedev@gmail.com>
2021-03-09 05:39:52 +0900

    Create gameaccount.js

---
+++ api-src/routes/gameaccount.js
@@ -0,0 +1,20 @@
+// created by shlifedev at 20210307 03:10.
+// 
+// author email : shlifedev@gmail.com
+
+const express  = require('express');
+const checkAuth = require('../middlewares/checkAuth');
+const router   = express.Router(); 
+// 라우터 URL 지정
+const URL = "/api/gameaccount";
+
+ 
+router.post('/');
+
+// gameid의 gameaccount가 본인의 것인지 인증하기 위한 verify 수단.verify 완료시 verified 값이 1이됨.
+router.post('/verify/:gameid', checkAuth);
+
+module.exports ={
+  router,
+  url : URL
+} ;
