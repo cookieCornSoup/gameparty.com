04000243a7ab6c0d6c7be774681141b52487c414
Zero Is Black <shlifedev@gmail.com>
2021-03-16 18:43:36 +0900

    Update navbar.js

--- react-client/src/components/common/navbar.js
+++ react-client/src/components/common/navbar.js
@@ -2,25 +2,31 @@
 import './navbar.css';
 import logoImage from '../../assets/GPNLogo.png'
 export default function Navbar(){
+
+    const navbarClick = (e)=>{
+        alert("준비중입니다.");
+        e.preventDefault();
+    }
+
     return (
         <navbar class="navbar">
             <div class="navbar-container">
                 <div class="navbar-left">
                     <img src={logoImage} alt="로고 이미지" className="logo"/>
                     <ul className="menu-list">
-                        <a href="">
+                        <a href="" onClick={navbarClick}>
                             <li className="menu-item">Party</li>
                         </a>
-                        <a href="">
+                        <a href="" onClick={navbarClick}>
                             <li className="menu-item">Matching</li>
                         </a>
-                        <a href="">
+                        <a href="" onClick={navbarClick}>
                             <li className="menu-item">Service</li>
                         </a>
-                        <a href="">
+                        <a href="" onClick={navbarClick}>
                             <li className="menu-item">Download</li>
                         </a>
-                        <a href="">
+                        <a href="" onClick={navbarClick}>
                             <li className="menu-item">Contact</li>
                         </a>
                     </ul>
