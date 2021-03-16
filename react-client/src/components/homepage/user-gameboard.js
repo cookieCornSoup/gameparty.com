89ff50cdd22cb04e76f9b4f21ab5b09c4b64f6cb
Zero Is Black <shlifedev@gmail.com>
2021-03-16 22:39:33 +0900

    react homepage

--- react-client/src/components/homepage/user-gameboard.js
+++ react-client/src/components/homepage/user-gameboard.js
@@ -2,9 +2,16 @@ import './user-gameboard.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import userIcon from '../../assets/user-icon.png'
 import { Button, ButtonToolbar } from 'react-bootstrap';
+ 
+import { faUser } from "@fortawesome/free-solid-svg-icons" 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export function GameBoardCard(props) {
     return (<>
+
+    
         <div className="g-match-card">
+            
+        
             <div className="match-update-date">
             방금 전
             </div>
@@ -12,11 +19,12 @@ export function GameBoardCard(props) {
             솔로랭크 골드3 원딜구함
             </div>
             <div className="match-description">
-            원딜분 구해요
+            잘하시는 원딜분 구합니다. 캐리해주시면 정말 감사하겠습니다. 글씨 길이좀 테스트하느라 좀 더 쓸게요 ㅋㅋ
             </div> 
-            <div className="match-bottom-wrapper">
-            <Button>입장</Button>
-            <h4>작성자이름</h4>
+            <div className="match-bottom-wrapper">  
+            <FontAwesomeIcon icon={faUser} className="match-user-count-ico" />
+            <p className="match-user-count"> 1/5명 모집됨</p>  
+            <Button className="match-join-btn">입장</Button>
             </div> 
         </div>
     </>)
@@ -30,6 +38,9 @@ export function GameBoard(props) {
                     <GameBoardCard></GameBoardCard>
                     <GameBoardCard></GameBoardCard>
                     <GameBoardCard></GameBoardCard> 
+                    <GameBoardCard></GameBoardCard>
+                    <GameBoardCard></GameBoardCard>
+                    <GameBoardCard></GameBoardCard>
                     <GameBoardCard></GameBoardCard>
                     <GameBoardCard></GameBoardCard>
                     <GameBoardCard></GameBoardCard> 
@@ -58,7 +69,7 @@ export default function BoardContainer() {
                     <Col xs={12}>
                         <div className="gameboard-bar">
                             <div className="g-board-bar-left">
-                                <img src={userIcon} alt="Img" class="g-board-bar-icon"></img>
+                                <FontAwesomeIcon icon={faUser} className="g-board-match-icon" size="2x"  />
                                 <h3>유저 매치</h3>
                             </div>
                             <div className="g-board-bar-right">
