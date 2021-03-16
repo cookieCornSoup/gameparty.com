b2401a93c5823446a9c0176b02ddcee95c30003b
Zero Is Black <shlifedev@gmail.com>
2021-03-16 23:02:59 +0900

    유저 게임보드 프로퍼티 추가

--- react-client/src/components/homepage/user-gameboard.js
+++ react-client/src/components/homepage/user-gameboard.js
@@ -2,48 +2,99 @@ import './user-gameboard.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import userIcon from '../../assets/user-icon.png'
 import { Button, ButtonToolbar } from 'react-bootstrap';
- 
-import { faUser } from "@fortawesome/free-solid-svg-icons" 
+
+import { faUser } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export function GameBoardCard(props) {
-    return (<>
+    const sampleData = [
+        {
+            matchUpdateDate: 30,
+            matchTitle: "같이 게임하실 분?",
+            matchDescription: "솔랭티어 브론즈 이상 아무나 구해요",
+            matchUserMax: 5,
+            matchUserCurrent: 1
+        },
+        {
+            matchUpdateDate: 30,
+            matchTitle: "같이 게임하실 분?",
+            matchDescription: "솔랭티어 브론즈 이상 아무나 구해요",
+            matchUserMax: 5,
+            matchUserCurrent: 1
+        },
+        {
+            matchUpdateDate: 30,
+            matchTitle: "같이 게임하실 분?",
+            matchDescription: "솔랭티어 브론즈 이상 아무나 구해요",
+            matchUserMax: 5,
+            matchUserCurrent: 1
+        },
+        {
+            matchUpdateDate: 30,
+            matchTitle: "같이 게임하실 분?",
+            matchDescription: "솔랭티어 브론즈 이상 아무나 구해요",
+            matchUserMax: 5,
+            matchUserCurrent: 1
+        },
+        {
+            matchUpdateDate: 30,
+            matchTitle: "같이 게임하실 분?",
+            matchDescription: "솔랭티어 브론즈 이상 아무나 구해요",
+            matchUserMax: 5,
+            matchUserCurrent: 1
+        },
+        {
+            matchUpdateDate: 30,
+            matchTitle: "같이 게임하실 분?",
+            matchDescription: "솔랭티어 브론즈 이상 아무나 구해요",
+            matchUserMax: 5,
+            matchUserCurrent: 1
+        }
+    ]
 
-    
+    let matchUpdateDateText = "알 수 없음";
+    if (props.matchUpdateDate <= 30) {
+        matchUpdateDateText = "방금 전";
+    }
+    else {
+        let minute = Math.floor((props.matchUpdateDate / 60));
+        matchUpdateDateText = minute + "분 전";
+    }
+
+    return (<>
         <div className="g-match-card">
-            
-        
             <div className="match-update-date">
-            방금 전
+                {matchUpdateDateText}
             </div>
             <div className="match-title">
-            솔로랭크 골드3 원딜구함
+                {props.matchTitle}
             </div>
             <div className="match-description">
-            잘하시는 원딜분 구합니다. 캐리해주시면 정말 감사하겠습니다. 글씨 길이좀 테스트하느라 좀 더 쓸게요 ㅋㅋ
-            </div> 
-            <div className="match-bottom-wrapper">  
-            <FontAwesomeIcon icon={faUser} className="match-user-count-ico" />
-            <p className="match-user-count"> 1/5명 모집됨</p>  
-            <Button className="match-join-btn">입장</Button>
-            </div> 
+                {props.matchDescription}
+            </div>
+            <div className="match-bottom-wrapper">
+                <FontAwesomeIcon icon={faUser} className="match-user-count-ico" />
+                <p className="match-user-count"> {props.matchUserCurrent}/{props.matchUserMax}명 모집됨</p>
+                <Button className="match-join-btn">입장</Button>
+            </div>
         </div>
     </>)
 
 }
+
+
 export function GameBoard(props) {
     return (
         <>
             <Container className="g-board-current-match-container">
                 <div className="g-board-current-match">
-                    <GameBoardCard></GameBoardCard>
-                    <GameBoardCard></GameBoardCard>
-                    <GameBoardCard></GameBoardCard> 
-                    <GameBoardCard></GameBoardCard>
-                    <GameBoardCard></GameBoardCard>
-                    <GameBoardCard></GameBoardCard>
-                    <GameBoardCard></GameBoardCard>
-                    <GameBoardCard></GameBoardCard>
-                    <GameBoardCard></GameBoardCard> 
+                    <GameBoardCard matchUserMax={5} matchUserCurrent={1} matchUpdateDate={20} matchDescription="나보다 잘하는 사람 문상 1억원 드림 진짜임 믿고오셈 내가 캐리해줌" matchTitle="같이 게임할 실버유저 구함"></GameBoardCard>
+                    <GameBoardCard matchUserMax={5} matchUserCurrent={1} matchUpdateDate={20} matchDescription="나보다 잘하는 사람 문상 1억원 드림 진짜임 믿고오셈 내가 캐리해줌" matchTitle="같이 게임할 실버유저 구함"></GameBoardCard>
+                    <GameBoardCard matchUserMax={5} matchUserCurrent={1} matchUpdateDate={20} matchDescription="나보다 잘하는 사람 문상 1억원 드림 진짜임 믿고오셈 내가 캐리해줌" matchTitle="같이 게임할 실버유저 구함"></GameBoardCard>
+                    <GameBoardCard matchUserMax={5} matchUserCurrent={1} matchUpdateDate={20} matchDescription="나보다 잘하는 사람 문상 1억원 드림 진짜임 믿고오셈 내가 캐리해줌" matchTitle="같이 게임할 실버유저 구함"></GameBoardCard>
+                    <GameBoardCard matchUserMax={5} matchUserCurrent={1} matchUpdateDate={20} matchDescription="나보다 잘하는 사람 문상 1억원 드림 진짜임 믿고오셈 내가 캐리해줌" matchTitle="같이 게임할 실버유저 구함"></GameBoardCard>
+                    <GameBoardCard matchUserMax={5} matchUserCurrent={1} matchUpdateDate={20} matchDescription="나보다 잘하는 사람 문상 1억원 드림 진짜임 믿고오셈 내가 캐리해줌" matchTitle="같이 게임할 실버유저 구함"></GameBoardCard>
+                    <GameBoardCard matchUserMax={5} matchUserCurrent={1} matchUpdateDate={20} matchDescription="나보다 잘하는 사람 문상 1억원 드림 진짜임 믿고오셈 내가 캐리해줌" matchTitle="같이 게임할 실버유저 구함"></GameBoardCard>
+
                 </div>
             </Container>
         </>
@@ -69,7 +120,7 @@ export default function BoardContainer() {
                     <Col xs={12}>
                         <div className="gameboard-bar">
                             <div className="g-board-bar-left">
-                                <FontAwesomeIcon icon={faUser} className="g-board-match-icon" size="2x"  />
+                                <FontAwesomeIcon icon={faUser} className="g-board-match-icon" size="2x" />
                                 <h3>유저 매치</h3>
                             </div>
                             <div className="g-board-bar-right">
