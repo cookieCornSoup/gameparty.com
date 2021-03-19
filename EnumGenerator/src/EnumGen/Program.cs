cf9912a03aae129d82a315bc941db1435415ac04
Zero Is Black <shlifedev@gmail.com>
2021-03-20 01:47:29 +0900

    args test

--- EnumGenerator/src/EnumGen/Program.cs
+++ EnumGenerator/src/EnumGen/Program.cs
@@ -15,16 +15,24 @@ namespace EnumGenerator
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("인자 개수 : " + args.Length);
+
             string path = @"..\Enum.json";
-            string readedJson = System.IO.File.ReadAllText(path);  
+            string readedJson = System.IO.File.ReadAllText(path);
+            if (args.Length == 0)
+            {
+
+            }
+
             SerializedEnum serializedEnum = Newtonsoft.Json.JsonConvert.DeserializeObject<SerializedEnum>(readedJson);
-            serializedEnum.Datas = serializedEnum.Datas.OrderBy(x =>x.Value).ToList(); 
-            System.IO.File.WriteAllText(@"..\Enum_sorted.json", JValue.Parse(Newtonsoft.Json.JsonConvert.SerializeObject(serializedEnum)).ToString(Formatting.Indented)); 
+            serializedEnum.Datas = serializedEnum.Datas.OrderBy(x => x.Value).ToList();
+            System.IO.File.WriteAllText(@"..\Enum_sorted.json", JValue.Parse(Newtonsoft.Json.JsonConvert.SerializeObject(serializedEnum)).ToString(Formatting.Indented));
             KotlinEnumGen kotlin = new KotlinEnumGen();
             JSEnumGen js = new JSEnumGen();
 
             kotlin.Generate(serializedEnum, GeneratedCodeProccessor);
             js.Generate(serializedEnum, GeneratedCodeProccessor);
+
         }
     }
 }
